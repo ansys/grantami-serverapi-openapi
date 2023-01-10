8c6a0b4eef13b825dfcda0a22940abade9139917
Andy-Grigg <Andy-Grigg@users.noreply.github.com>
2023-01-10 18:51:10 +0000

    Update client library

---
+++ ansys-grantami-serverapi-openapi/src/ansys/grantami/serverapi_openapi/api/__init__.py
@@ -0,0 +1,31 @@
+# flake8: noqa
+
+# import API ABC
+from ansys.openapi.common import ApiBase
+
+# import apis into api package
+from .aggregation_api import AggregationApi
+from .data_export_api import DataExportApi
+from .database_api import DatabaseApi
+from .integration_api import IntegrationApi
+from .list_item_api import ListItemApi
+from .list_management_api import ListManagementApi
+from .list_permissions_api import ListPermissionsApi
+from .schema_api import SchemaApi
+from .schema___attributes_api import SchemaAttributesApi
+from .schema___constants_api import SchemaConstantsApi
+from .schema___databases_api import SchemaDatabasesApi
+from .schema___discrete_types___discrete_values_api import SchemaDiscreteTypesDiscreteValuesApi
+from .schema___discrete_types_api import SchemaDiscreteTypesApi
+from .schema___layout_sections_api import SchemaLayoutSectionsApi
+from .schema___layouts_api import SchemaLayoutsApi
+from .schema___parameters_api import SchemaParametersApi
+from .schema___profile_tables_api import SchemaProfileTablesApi
+from .schema___profiles_api import SchemaProfilesApi
+from .schema___standard_names_api import SchemaStandardNamesApi
+from .schema___subsets_api import SchemaSubsetsApi
+from .schema___tables_api import SchemaTablesApi
+from .schema___units_api import SchemaUnitsApi
+from .search_api import SearchApi
+from .status_api import StatusApi
+from .suggestions_api import SuggestionsApi
