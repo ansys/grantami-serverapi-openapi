f75418f472d803452033e87842c0731daa601499
da1910 <da1910@users.noreply.github.com>
2023-10-11 12:06:36 +0000

    Generate Client Library code - Automated

--- ansys-grantami-serverapi-openapi/tests/test_general.py
+++ ansys-grantami-serverapi-openapi/tests/test_general.py
@@ -5,6 +5,9 @@ import pytest
 src_folder = Path(__file__) / "../../src"
 files = list(src_folder.rglob("*.py"))
 
-@pytest.mark.parametrize("file", files, ids=[f.relative_to(src_folder).as_posix() for f in files])
+
+@pytest.mark.parametrize(
+    "file", files, ids=[f.relative_to(src_folder).as_posix() for f in files]
+)
 def test_all_code_valid(file):
     py_compile.compile(file=file, doraise=True)
