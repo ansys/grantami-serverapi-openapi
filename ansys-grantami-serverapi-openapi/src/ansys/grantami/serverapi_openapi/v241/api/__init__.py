82443dbddf2372286769fad559f0b1283459ca1b
Doug Addy <doug.addy@ansys.com>
2025-01-17 17:03:21 +0000

    Add initial sketch of multiple-version package

---
+++ ansys-grantami-serverapi-openapi/src/ansys/grantami/serverapi_openapi/v241/api/__init__.py
@@ -0,0 +1,44 @@
+# flake8: noqa
+
+# import API ABC
+from ansys.openapi.common import ApiBase
+
+# import apis into api package
+from .aggregation_api import AggregationApi
+from .data_export_api import DataExportApi
+from .database_api import DatabaseApi
+from .integration_api import IntegrationApi
+from .job_queue_api import JobQueueApi
+from .license_api import LicenseApi
+from .list_item_api import ListItemApi
+from .list_management_api import ListManagementApi
+from .list_permissions_api import ListPermissionsApi
+from .records___record_histories_api import RecordsRecordHistoriesApi
+from .records___record_versions_api import RecordsRecordVersionsApi
+from .schema_api import SchemaApi
+from .schema___attributes_api import SchemaAttributesApi
+from .schema___configurations_api import SchemaConfigurationsApi
+from .schema___constants_api import SchemaConstantsApi
+from .schema___data_rules_api import SchemaDataRulesApi
+from .schema___databases_api import SchemaDatabasesApi
+from .schema___discrete_types___discrete_values_api import (
+    SchemaDiscreteTypesDiscreteValuesApi,
+)
+from .schema___discrete_types_api import SchemaDiscreteTypesApi
+from .schema___exporters_api import SchemaExportersApi
+from .schema___expressions_api import SchemaExpressionsApi
+from .schema___help_files_api import SchemaHelpFilesApi
+from .schema___home_pages_api import SchemaHomePagesApi
+from .schema___layout_sections_api import SchemaLayoutSectionsApi
+from .schema___layouts_api import SchemaLayoutsApi
+from .schema___parameters_api import SchemaParametersApi
+from .schema___profile_tables_api import SchemaProfileTablesApi
+from .schema___profiles_api import SchemaProfilesApi
+from .schema___record_link_groups_api import SchemaRecordLinkGroupsApi
+from .schema___standard_names_api import SchemaStandardNamesApi
+from .schema___subsets_api import SchemaSubsetsApi
+from .schema___tables_api import SchemaTablesApi
+from .schema___units_api import SchemaUnitsApi
+from .search_api import SearchApi
+from .selection_searches_api import SelectionSearchesApi
+from .status_api import StatusApi
